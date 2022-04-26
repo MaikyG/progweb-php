609854697ab36995e65efcddddce1bbbf02f59da
MaikyG <maikygabriel3@gmail.com>
2022-04-26 15:50:31 -0300

    exercicio

---
+++ 03-exercicio.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio</title>
+</head>
+<body>
+    <?php
+    //variaveis
+    $data = "26/04/2022";
+    $nome = "Maiky";
+    $idade = 18;
+    $cidade = "São Paulo";
+
+    echo "<p> hoje é $data. $nome tem $idade anos e mora em $cidade  </p>";
+
+     ?>
+
+     
+</body>
+</html>
